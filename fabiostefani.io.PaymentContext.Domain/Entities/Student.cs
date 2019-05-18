2ba6909d31d29de7852e21c5ec6317685948c5be
fabiostefani@gmail.com <fabiostefani@gmail.com>
2019-05-18 07:20:19 -0300

    Exemplos de corrupção de código

--- fabiostefani.io.PaymentContext.Domain/Entities/Student.cs
+++ fabiostefani.io.PaymentContext.Domain/Entities/Student.cs
@@ -1,15 +1,38 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace fabiostefani.io.PaymentContext.Domain.Entities
 {
     public class Student
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Document { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public List<Subscription> Subscriptions { get; set; }
+        private IList<Subscription> _subcriptions;
+        public Student(string firstName, string lastName, string document, string email)
+        {
+            _subcriptions = new List<Subscription>();
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email; 
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get {return _subcriptions.ToArray(); } }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            //Se ja tiver uma assinatura ativa, cancela e não adiciona uma nova
+            //Cancela todas as outras assinaturas e coloca esta como principal
+            foreach (var sub in Subscriptions)
+            {
+                sub.Inactivate();
+            }
+            _subcriptions.Add(subscription);
+        }
     }
 
 }
---
+++ fabiostefani.io.PaymentContext.Tests/Entities/StudentsTests.cs
@@ -0,0 +1,24 @@
+using System;
+using fabiostefani.io.PaymentContext.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fabiostefani.io.PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var student = new Student("Fabio", "de Stefani", "123456", "fabioste@fabiostefani.io");            
+        }
+
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Fabio", "de Stefani", "123456", "fabioste@fabiostefani.io");            
+            student.AddSubscription(subscription);
+        }
+    }
+}
